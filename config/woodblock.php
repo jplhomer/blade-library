1f586174c41a89b21927cd4ff614cbdd6bf39ae0
Josh Larson <jplhomer@gmail.com>
2020-06-29 07:37:32 -0500

    Add path config

--- config/woodblock.php
+++ config/woodblock.php
@@ -2,4 +2,15 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Blade Library Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Blade Library will be accessible from.
+    | Feel free to change this path to anything you like.
+    |
+    */
+
+    'path' => '/library',
 ];
