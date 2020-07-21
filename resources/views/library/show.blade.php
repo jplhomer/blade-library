3aadcd66729201db0abf4d88eeb17aa5b83175ff
Josh Larson <jplhomer@gmail.com>
2020-07-20 22:48:31 -0500

    Tiny tweaks

--- resources/views/library/show.blade.php
+++ resources/views/library/show.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blade Library</title>
     @livewireStyles
+
+    {{-- TODO: Allow user to pull their own layout, or insert scripts/styles --}}
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
 
-@include($view)
+<div style="padding: .5em">
+    @include($view)
+</div>
 
 @livewireScripts
 </body>
